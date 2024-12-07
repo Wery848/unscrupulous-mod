c4f7fdce15d5f14f8ff8d013e7fdc0daa8c366c0
Andy D <wery8484@gmail.com>
2024-12-07 16:35:02 -0500

    Added Block to the game
    ModBlocks.java was added and all of the necessary methods to make blocks in this version.

---
+++ src/main/java/com/github/Wery848/unscrupulous/block/ModBlocks.java
@@ -0,0 +1,43 @@
+package com.github.Wery848.unscrupulous.block;
+
+import com.github.Wery848.unscrupulous.UnscrupulousMod;
+import com.github.Wery848.unscrupulous.item.ModItems;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModBlocks {
+    // DeferredRegister for Blocks
+    public static final DeferredRegister.Blocks BLOCKS =
+            DeferredRegister.createBlocks(UnscrupulousMod.MODID);
+
+    // Registry Blocks
+    public static final DeferredBlock<Block> SOUL_STONE_ORE = registerBlockAndItem("soul_stone_ore",
+            BlockBehaviour.Properties.of()
+                    .requiresCorrectToolForDrops()
+                    .strength(1.5f, 6.0f)
+                    .sound(SoundType.STONE)
+                    .mapColor(MapColor.STONE)
+                    );
+
+    // Utility Methods
+    // New and improved helper method
+    private static DeferredBlock<Block> registerBlockAndItem(String name, BlockBehaviour.Properties props) {
+        DeferredBlock<Block> ret = BLOCKS.registerBlock(
+            name,
+            Block::new,
+            props
+        );
+        ModItems.ITEMS.registerSimpleBlockItem(ret);
+        return ret;
+    }
+
+    // Registers the BLOCKS deferredregister
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
--- src/main/java/com/github/Wery848/unscrupulous/item/ModItems.java
+++ src/main/java/com/github/Wery848/unscrupulous/item/ModItems.java
@@ -9,19 +9,12 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-public class Items {
+public class ModItems {
     // Deferred Register for items
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(UnscrupulousMod.MODID);
 
-    // Item registries
-    // Old Registry Style
-//    public static final DeferredItem<Item> SOUL_STONE = ITEMS.register("soul_stone",
-//            () -> new Item(new Item.Properties().setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(UnscrupulousMod.MODID, "soul_stone")))
-//            ));
-
     // New & Improved - 12/3/24
     public static final DeferredItem<Item> SOUL_STONE = createItemWithProps("soul_stone", new Item.Properties());
-
 
     // Register method for items
     public static void register(IEventBus eventBus) {
@@ -29,10 +22,10 @@ public class Items {
     }
 
     // Utility Methods
-
     // Register Item (adds default resource key)
-    private static DeferredItem<Item> createItemWithProps(String name, Item.Properties itemProps) {
+    public static DeferredItem<Item> createItemWithProps(String name, Item.Properties itemProps) {
         return ITEMS.register(name,
                 () -> new Item(itemProps.setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(UnscrupulousMod.MODID, name)))));
     }
+
 }
